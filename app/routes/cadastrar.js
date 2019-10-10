0e8b25279b7774bbd8191680dacacfbe8f1254d5
Daniela Gonzalez <dani.gonzalez26012001@gmail.com>
2019-10-10 01:33:50 -0300

    Subindo Site

--- app/routes/cadastrar.js
+++ app/routes/cadastrar.js
@@ -1,11 +1,22 @@
 module.exports = function(app){
-  
   app.get('/cadastrar', function(req, res){
-    app.app.controllers.cadastrar_usuario.form_add_usuario(app, req, res);
-  });
+    res.render('cadastrar/cadastrar');
+  })
+ 
+ 
+  app.get('/teste', function(req, res){
+    let db = app.config.dbConnection;
 
-  app.get('/cadastrar/sucesso', function(req, res){
-    app.app.controllers.cadastrar_usuario.add_usuario(app, req, res);
+    db.collection('users').get()
+      .then((snapshot) => {
+          snapshot.forEach((doc) => {
+          // console.log(doc.id, '=>', doc.data());
+          });
+          res.render('home/index');
+      })
+      .catch((err) => {
+          console.log('Error getting documents', err);
+      });
   });
 
 }
